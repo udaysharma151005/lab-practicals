5ed799676bb37230b6d797a0daa732f496e59345
udaysharma151005 <114711792+udaysharma151005@users.noreply.github.com>
2023-01-08 19:16:07 +0530

    Add files via upload

---
+++ C assingment/find_power_of_any_number.c
@@ -0,0 +1,21 @@
+// C program to find power of any number using for loop.
+#include <stdio.h>
+
+int main(void) {
+  int base, exponent, result = 1;
+
+  printf("Enter base: ");
+  scanf("%d", &base);
+
+  printf("Enter exponent: ");
+  scanf("%d", &exponent);
+
+  for (int i = 1; i <= exponent; ++i) {
+    result *= base;
+  }
+
+  printf("%d to the power of %d is %d\n", base, exponent, result);
+
+  return 0;
+}
+
---
+++ C assingment/palidrom_in_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   char inputArray[100], reversedArray[100];
+   printf("Enter the string for palindrome check \n");
+   scanf("%s", inputArray);
+   strcpy(reversedArray, inputArray);
+   strrev(reversedArray);
+   if(strcmp(inputArray, reversedArray) == 0 )
+      printf("%s is a palindrome.\n", inputArray);
+   else
+      printf("%s is not a palindrome.\n", inputArray);
+   getch();
+   return 0;
+}
